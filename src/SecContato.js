d779301ef6cec70d9bd0e0e0ab4eebfa6a133099
Eduardo Santos <76977508+dudukb@users.noreply.github.com>
2021-03-13 12:53:17 -0300

    Update SecContato.js

--- src/SecContato.js
+++ src/SecContato.js
@@ -1,27 +1,26 @@
 import "./secContato.scss";
 import contact1 from "./assets/contato-1.png";
 import contact2 from "./assets/contato-2.png";
+
 const SecContato = () => {
   return (
     <section class="container">
       {
       <div class="teste">
-      <h1>Tem um projeto em mente?</h1>
-          <h1>Entre em contato!</h1>
+     <h1>Tem um <span class="texto-colorido">projeto</span> em mente?<br></br>
+          Entre em <span class="texto-colorido">contato!</span>
+     </h1>
       </div>
       }
-      {/* Icones */}
+
       {
         <div>
-          <img id="img1"src={contact1} />
-
-          <img id="img2" src={contact2} />
-
-          
-          
+          <img align="left" id="img1" src={contact1} />
+          <br /> <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+          <img align="right" id="img2" src={contact2} />
         </div>
-
       }
+
     </section>
   );
 };
