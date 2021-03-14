1076b0f024e6e997fa64a516514f20e670ea8340
Jesús Sánchez <sanchezbgsus@gmail.com>
2021-03-14 16:10:18 -0400

    Seção Trabalho. PRONTO O SITE

--- src/SecTrabalho.js
+++ src/SecTrabalho.js
@@ -1,41 +1,71 @@
 // import Project from "./Project";
+import { useState } from "react";
+import ReactCardFlip from "react-card-flip";
 import "./SecTrabalho.scss";
 import LOG from "./assets/img01.jpg";
 import LAB from "./assets/img02.jpg";
 import DIN from "./assets/img03.jpg";
-import LINE from "./assets/rectline.svg";
-import Flippy, { FrontSide, BackSide } from 'react-flippy';
 
 const SecTrabalho = () => {
+  const [isFlipped, setIsFlipped] = useState(false);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+
+    setIsFlipped(!isFlipped);
+  };
+
   return (
     <section id="trabalho">
-      <h2>O nosso <span id="h2stU">trabalho</span></h2>
-      <img id="rectline" src={LINE} alt=""></img>
+      <h2>
+        O nosso <span className="highlighted">trabalho</span>
+      </h2>
+      <hr />
+
+      <ReactCardFlip isFlipped={isFlipped} flipDirection="vertical">
+        <div className="card-front">
+          <p>Lógica de programação para o público infanto-juvenil.</p>
+          <div className="toggler" onClick={handleClick}></div>
+        </div>
+
+        <div className="card-back">
+          <img src={LOG} alt="" />
+          <div className="toggler" onClick={handleClick}></div>
+        </div>
+      </ReactCardFlip>
+
+      <ReactCardFlip isFlipped={isFlipped} flipDirection="vertical">
+        <div className="card-front">
+          <p>
+            Laboratório Estóico: aplicações web simples e/ou imersão no mundo da
+            inteligência artificial
+          </p>
+          <div className="toggler" onClick={handleClick}></div>
+        </div>
+
+        <div className="card-back">
+          <img src={LAB} alt="" />
+          <div className="toggler" onClick={handleClick}></div>
+        </div>
+      </ReactCardFlip>
+
+      <ReactCardFlip isFlipped={isFlipped} flipDirection="vertical">
+        <div className="card-front">
+          <p>
+            Dinâmicas e minicursos visando o desenvolvimento da inteligência
+            emocional
+          </p>
+          <div className="toggler" onClick={handleClick}></div>
+        </div>
+
+        <div className="card-back">
+          <img src={DIN} alt="" />
+          <div className="toggler" onClick={handleClick}></div>
+        </div>
+      </ReactCardFlip>
     </section>
   );
 };
-
-/*
-render() {
-  // .. return
-<Flippy
-  flipOnHover={false} // default false
-  flipOnClick={true} // default false
-  flipDirection="horizontal" // horizontal or vertical
-  ref={(r) => this.flippy = r} // to use toggle method like this.flippy.toggle()
-  // if you pass isFlipped prop component will be controlled component.
-  // and other props, which will go to div
-  style={{ id="rect1" }}
-  <FrontSide 
-  img id="log" src={LOG} alt="Logic">
-  </FrontSide>
-  <BackSide
-  <p><span id="description1">Lógica de programação para o público infanto-juvenil></span></p>
-  </BackSide>
-</Flippy>
-}
-*/
-
 
 /*
         <description1>Lógica de programação para o público infanto-juvenil></description1>
